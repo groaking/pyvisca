a313cba65cb1b8f4a4c49f144ddc9ec654526954
groaking <93555329+groaking@users.noreply.github.com>
2024-06-19 20:57:04 +0700

    Added setup.py as miscellaneous

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='pyvisca',
+    version='0.0.2',
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=[],
+)
+
+# Compile with:
+# python setup.py sdist upload -r pypi
